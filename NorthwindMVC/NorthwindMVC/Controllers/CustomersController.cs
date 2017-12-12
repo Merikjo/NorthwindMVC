8aae9551331d92da0deb6d018389f235a91506ff
Johanna Merikanto <johanna.merikanto2@pointcollege.fi>
2017-12-12 11:46:54 +0200

    Luotu uusi projekti, joka hakee ja tallentaa tietoa Azuren Northwind tietokannasta. Lisätty tietokanta Entity Frameworkin avulla. Toteutettu bootstrap dashboard, josta linkit eri sivustoille.

---
+++ NorthwindMVC/NorthwindMVC/Controllers/CustomersController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NorthwindMVC.Models;
+
+namespace NorthwindMVC.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private NorthwindDataEntities db = new NorthwindDataEntities();
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            List<Customers> model = new List<Customers>();
+            try
+            {
+                NorthwindDataEntities entities = new NorthwindDataEntities();
+                model = entities.Customers.ToList();
+                entities.Dispose();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = ex.GetType() + ": " + ex.Message;
+            }
+
+            return View(model);
+        }
+
+
+        public ActionResult GetOrders(string id)
+        {
+            NorthwindDataEntities entities = new NorthwindDataEntities();
+
+            List<Orders> orders = (from o in entities.Orders
+                                   where o.CustomerID == id
+                                   select o).ToList();
+
+
+            entities.Dispose();
+
+            List<SimpleOrderData> result = new List<SimpleOrderData>();
+            foreach (Orders order in orders)
+            {
+                SimpleOrderData data = new SimpleOrderData();
+                data.CustomerId = order.CustomerID;
+                data.OrderId = order.OrderID;
+                data.OrderDate = order.OrderDate.ToString();
+                result.Add(data);
+            }
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+
+        // GET: Customers/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customers customers = db.Customers.Find(id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customers);
+        }
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CustomerID,CompanyName,ContactName,ContactTitle,Address,City,Region,PostalCode,Country,Phone,Fax")] Customers customers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customers);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customers);
+        }
+
+        // GET: Customers/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customers customers = db.Customers.Find(id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customers);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CustomerID,CompanyName,ContactName,ContactTitle,Address,City,Region,PostalCode,Country,Phone,Fax")] Customers customers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customers).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customers);
+        }
+
+        // GET: Customers/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customers customers = db.Customers.Find(id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customers);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Customers customers = db.Customers.Find(id);
+            db.Customers.Remove(customers);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
